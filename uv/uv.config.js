1bb60e75c1c2b30d5175ac521008ad8699d7d79a
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2025-11-09 12:35:04 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://uv.bare-server.net/",
+  bare: "/service/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
