a85dbda5f3f32acc74a554d3fe59990d19357994
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2025-11-09 12:53:37 -0600

    Update UV config to change bare service path

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/service/",
+  bare: "../api/service.js/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
